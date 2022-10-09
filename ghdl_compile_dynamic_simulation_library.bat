6fcefde888463b6dfbbb9fd4eb65d3f114443523
jari <jaarhonka@yahoo.com>
2022-10-09 08:10:01 +0300

    move fixed point library into the dynamic verification library as submodule

--- ghdl_compile_dynamic_simulation_library.bat
+++ ghdl_compile_dynamic_simulation_library.bat
@@ -4,21 +4,37 @@ SET project_root=%%F
 )
 SET source=%project_root%/../
 
-call %source%/math_library/ghdl_compile_math_library.bat
+rem call %source%/hVHDL_math_library/ghdl_compile_math_library.bat
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/hVHDL_memory_library/fpga_ram/ram_configuration/ram_configuration_16x1024_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/hVHDL_memory_library/fpga_ram/ram_read_port_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/state_variable/state_variable_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/lcr_filter_model/lcr_filter_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/inverter_model/inverter_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/power_supply_model/psu_inverter_simulation_models_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/power_supply_model/power_supply_simulation_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/multiplier/multiplier_base_types_18bit_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/multiplier/multiplier_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/sincos/sincos_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/simulation/testi/buck_converter_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/simulation/full_bridge_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/sincos/lut_generator_functions/sine_lut_generator_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/sincos/lut_sine_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/ac_motor_models/pmsm_electrical_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/ac_motor_models/pmsm_mechanical_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/ac_motor_models/permanent_magnet_motor_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/dynamic_simulation_library/ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/division/division_internal_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/division/division_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/first_order_filter/first_order_filter_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/pi_controller/pi_controller_pkg.vhd
 
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/ab_to_abc_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd
 
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/state_variable/state_variable_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/lcr_filter_model/lcr_filter_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/inverter_model/inverter_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/power_supply_model/psu_inverter_simulation_models_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/power_supply_model/power_supply_simulation_model_pkg.vhd
 
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/simulation/testi/buck_converter_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/simulation/full_bridge_model_pkg.vhd
+
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/ac_motor_models/pmsm_electrical_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/ac_motor_models/pmsm_mechanical_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/ac_motor_models/permanent_magnet_motor_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_dynamic_model_verification_library/ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_pkg.vhd
--- vunit_run_dynamic_simulation_library.py
+++ vunit_run_dynamic_simulation_library.py
@@ -8,18 +8,18 @@ ROOT = Path(__file__).resolve().parent
 VU = VUnit.from_argv()
 
 mathlib = VU.add_library("math_library")
-mathlib.add_source_files(ROOT / "../math_library/multiplier/multiplier_base_types_22bit_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/multiplier/multiplier_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/sincos/sincos_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/pi_controller/pi_controller_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/abc_to_ab_transform/ab_to_abc_transform_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/multiplier/multiplier_base_types_22bit_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/multiplier/multiplier_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/sincos/sincos_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/pi_controller/pi_controller_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/ab_to_abc_transform_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd") 
 
-mathlib.add_source_files(ROOT / "../math_library/multiplier/simulation/tb_multiplier.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_simulation/tb_abc_to_ab_transform.vhd") 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_simulation/tb_ab_to_dq_transforms.vhd")
+mathlib.add_source_files(ROOT / "hVHDL_math_library/multiplier/simulation/tb_multiplier.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_simulation/tb_abc_to_ab_transform.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_simulation/tb_ab_to_dq_transforms.vhd")
 
 mathlib.add_source_files(ROOT / "state_variable/state_variable_pkg.vhd")
 mathlib.add_source_files(ROOT / "ac_motor_models/pmsm_electrical_model_pkg.vhd")
@@ -30,7 +30,7 @@ mathlib.add_source_files(ROOT / "ac_motor_models/field_oriented_motor_control/fi
 mathlib.add_source_files(ROOT / "lcr_filter_model/lcr_filter_model_pkg.vhd")
 mathlib.add_source_files(ROOT / "lcr_filter_model/lcr_filter_simulation/tb_lcr_filter.vhd")
 
-mathlib.add_source_files(ROOT / "../math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd") 
+mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd") 
 mathlib.add_source_files(ROOT / "ac_motor_models/simulate_permanent_magnet_synchronous_machine/tb_permanent_magnet_synchronous_machine_model.vhd")
 mathlib.add_source_files(ROOT / "ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_simulation/tb_field_oriented_motor_control.vhd")
 
@@ -46,8 +46,8 @@ mathlib.add_source_files(ROOT / "state_variable/simulation/tb_state_variable.vhd
 simulation = VU.add_library("real_simulation")
 simulation.add_source_files(ROOT / "simulation/testi" / "*.vhd")
 simulation.add_source_files(ROOT / "simulation" / "*.vhd")
-simulation.add_source_files(ROOT / "../math_library/multiplier/multiplier_base_types_22bit_pkg.vhd") 
-simulation.add_source_files(ROOT / "../math_library/multiplier/multiplier_pkg.vhd") 
+simulation.add_source_files(ROOT / "hVHDL_math_library/multiplier/multiplier_base_types_22bit_pkg.vhd") 
+simulation.add_source_files(ROOT / "hVHDL_math_library/multiplier/multiplier_pkg.vhd") 
 
 
 VU.main()
