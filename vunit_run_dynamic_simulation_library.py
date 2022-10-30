4a4d0ee0129ff60dac6a0e37efad14db8b863dce
jari <jaarhonka@yahoo.com>
2022-10-30 14:50:40 +0200

    move all testbenches under a single testbenches folder

--- vunit_run_dynamic_simulation_library.py
+++ vunit_run_dynamic_simulation_library.py
@@ -17,9 +17,6 @@ mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/abc_to
 mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd") 
 mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd") 
 
-mathlib.add_source_files(ROOT / "hVHDL_math_library/multiplier/simulation/tb_multiplier.vhd") 
-mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_simulation/tb_abc_to_ab_transform.vhd") 
-mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_simulation/tb_ab_to_dq_transforms.vhd")
 
 mathlib.add_source_files(ROOT / "state_variable/state_variable_pkg.vhd")
 mathlib.add_source_files(ROOT / "ac_motor_models/pmsm_electrical_model_pkg.vhd")
@@ -28,20 +25,14 @@ mathlib.add_source_files(ROOT / "ac_motor_models/permanent_magnet_motor_model_pk
 mathlib.add_source_files(ROOT / "ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_pkg.vhd")
 
 mathlib.add_source_files(ROOT / "lcr_filter_model/lcr_filter_model_pkg.vhd")
-mathlib.add_source_files(ROOT / "lcr_filter_model/lcr_filter_simulation/tb_lcr_filter.vhd")
 
 mathlib.add_source_files(ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd") 
-mathlib.add_source_files(ROOT / "ac_motor_models/simulate_permanent_magnet_synchronous_machine/tb_permanent_magnet_synchronous_machine_model.vhd")
-mathlib.add_source_files(ROOT / "ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_simulation/tb_field_oriented_motor_control.vhd")
 
 mathlib.add_source_files(ROOT / "inverter_model/inverter_model_pkg.vhd")
-mathlib.add_source_files(ROOT / "inverter_model/inverter_model_simulation/tb_inverter_model.vhd")
 
 mathlib.add_source_files(ROOT / "power_supply_model/power_supply_simulation_model_pkg.vhd")
 mathlib.add_source_files(ROOT / "power_supply_model/psu_inverter_simulation_models_pkg.vhd")
-mathlib.add_source_files(ROOT / "power_supply_model/power_supply_model_simulation/tb_power_supply_model.vhd")
 
-mathlib.add_source_files(ROOT / "state_variable/simulation/tb_state_variable.vhd")
 
 simulation = VU.add_library("real_simulation")
 simulation.add_source_files(ROOT / "hVHDL_math_library/multiplier/multiplier_base_types_22bit_pkg.vhd") 
@@ -52,10 +43,23 @@ math_library_22x22.add_source_files(ROOT / "hVHDL_math_library/multiplier/multip
 math_library_22x22.add_source_files(ROOT / "hVHDL_math_library/multiplier/multiplier_pkg.vhd")
 math_library_22x22.add_source_files(ROOT / "state_variable/state_variable_pkg.vhd")
 math_library_22x22.add_source_files(ROOT / "lcr_filter_model/lcr_filter_model_pkg.vhd")
-math_library_22x22.add_source_files(ROOT / "simulation/converter_models/grid_inverter_tb.vhd")
-math_library_22x22.add_source_files(ROOT / "simulation/converter_models/grid_inverter_current_step_tb.vhd")
 
 math_library_22x22.add_source_files(ROOT / "simulator_engine/simulation_configuration_pkg.vhd")
 math_library_22x22.add_source_files(ROOT / "simulator_engine/simulation_pkg.vhd")
+
+mathlib.add_source_files(ROOT / "hVHDL_math_library/multiplier/simulation/tb_multiplier.vhd") 
+
+# testbenches
+mathlib.add_source_files(            ROOT / "hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_simulation/tb_abc_to_ab_transform.vhd")
+mathlib.add_source_files(            ROOT / "hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_simulation/tb_ab_to_dq_transforms.vhd")
+
+mathlib.add_source_files(            ROOT / "testbenches/state_variable_simulation/tb_state_variable.vhd")
+math_library_22x22.add_source_files( ROOT / "testbenches/converter_models/grid_inverter_tb.vhd")
+math_library_22x22.add_source_files( ROOT / "testbenches/converter_models/grid_inverter_current_step_tb.vhd")
+mathlib.add_source_files(            ROOT / "testbenches/lcr_filter_simulation/tb_lcr_filter.vhd")
+mathlib.add_source_files(            ROOT / "testbenches/simulate_permanent_magnet_synchronous_machine/tb_permanent_magnet_synchronous_machine_model.vhd")
+mathlib.add_source_files(            ROOT / "testbenches/field_oriented_motor_control_simulation/tb_field_oriented_motor_control.vhd")
+mathlib.add_source_files(            ROOT / "testbenches/inverter_model_simulation/tb_inverter_model.vhd")
+mathlib.add_source_files(            ROOT / "testbenches/power_supply_model_simulation/tb_power_supply_model.vhd")
 
 VU.main()
