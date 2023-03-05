b0d9da7f6f3bb292565436e4c1f239381e15ae73
jari <jaarhonka@yahoo.com>
2023-03-06 00:03:35 +0200

    remove dependency to "simulator engine" files

--- testbenches/converter_models/cascaded_lcr_filters_tb.vhd
+++ testbenches/converter_models/cascaded_lcr_filters_tb.vhd
@@ -11,6 +11,7 @@ context vunit_lib.vunit_context;
     use work.lcr_filter_model_pkg.all;
     use work.simulation_pkg.all;
     use work.write_pkg.all;
+    use work.real_to_fixed_pkg.all;
 
 entity grid_inverter_current_step_tb is
   generic (runner_cfg : string);
@@ -25,8 +26,8 @@ architecture vunit_simulation of grid_inverter_current_step_tb is
     -- simulation specific signals ----
 
     constant L1_inductance : real := 1.0e-3;
-    constant L2_inductance : real := 1.0e-3;
-    constant L3_inductance : real := 1.0e-3;
+    constant L2_inductance : real := 4.0e-6;
+    constant L3_inductance : real := 4.0e-6;
 
     constant C1_capacitance : real := 10.0e-6;
     constant C2_capacitance : real := 3.3e-6;
@@ -35,16 +36,17 @@ architecture vunit_simulation of grid_inverter_current_step_tb is
     constant simulation_time_step : real := 0.3e-6;
     constant stoptime         : real := 2.0e-3;
     signal simulation_time    : real := 0.0;
+    constant int_radix : integer := int_word_length-1;
     ----
-    signal primary_lc : lcr_model_record := init_lcr_filter(inductance_is(1000.0e-6) , capacitance_is(10.0e-6) , resistance_is(0.2));
-    signal emi_lc_0   : lcr_model_record := init_lcr_filter(inductance_is(4.0e-6)    , capacitance_is(3.3e-6)  , resistance_is(50.0e-3));
-    signal emi_lc_1   : lcr_model_record := init_lcr_filter(inductance_is(4.0e-6)    , capacitance_is(7.0e-6)  , resistance_is(50.0e-3));
+
+    signal primary_lc : lcr_model_record := init_lcr_filter(L1_inductance , C1_capacitance , 200.0e-3 , simulation_time_step , int_radix);
+    signal emi_lc_0   : lcr_model_record := init_lcr_filter(L2_inductance , C2_capacitance , 50.0e-3  , simulation_time_step , int_radix);
+    signal emi_lc_1   : lcr_model_record := init_lcr_filter(L3_inductance , C3_capacitance , 50.0e-3  , simulation_time_step , int_radix);
 
     signal multiplier_1 : multiplier_record := init_multiplier;
     signal multiplier_2 : multiplier_record := init_multiplier;
     signal multiplier_3 : multiplier_record := init_multiplier;
     signal output_voltage   : real := 0.0;
-    signal input_voltage    : real := 325.0;
     signal inductor_current : real := 0.0;
 
 begin
@@ -64,6 +66,17 @@ begin
     stimulus : process(simulator_clock)
 
         file file_handler : text open write_mode is "grid_inverter_inductor_step.dat";
+        constant scale_value : real := 2.0**10;
+
+        impure function to_real
+        (
+            number : integer
+        )
+        return real
+        is
+        begin
+            return to_real(number , int_radix)*scale_value;
+        end to_real;
 
     begin
         if rising_edge(simulator_clock) then
@@ -72,9 +85,9 @@ begin
             create_multiplier(multiplier_2);
             create_multiplier(multiplier_3);
 
-            create_lcr_filter(primary_lc , multiplier_1 ,  get_inductor_current(emi_lc_0) , 0);
-            create_lcr_filter(emi_lc_0   , multiplier_2 ,  get_inductor_current(emi_lc_1) , get_capacitor_voltage(primary_lc));
-            create_lcr_filter(emi_lc_1   , multiplier_3 ,  0                              , get_capacitor_voltage(emi_lc_0));
+            create_lcr_filter(primary_lc , multiplier_1 , get_inductor_current(emi_lc_0) , 0                                 , int_radix);
+            create_lcr_filter(emi_lc_0   , multiplier_2 , get_inductor_current(emi_lc_1) , get_capacitor_voltage(primary_lc) , int_radix);
+            create_lcr_filter(emi_lc_1   , multiplier_3 , to_fixed(5.0/scale_value      , int_radix)                        , get_capacitor_voltage(emi_lc_0) , int_radix);
 
             if lcr_filter_calculation_is_ready(primary_lc) or simulation_counter = 0 then
                 request_lcr_filter_calculation(primary_lc);
@@ -83,17 +96,17 @@ begin
 
                 simulation_time <= simulation_time + simulation_time_step;
                 write_to(file_handler,(0 => simulation_time,
-                                       1 => real_voltage(get_inductor_current(primary_lc))  ,
-                                       2 => real_voltage(get_capacitor_voltage(primary_lc)) ,
-                                       3 => real_voltage(get_inductor_current(emi_lc_0))    ,
-                                       4 => real_voltage(get_capacitor_voltage(emi_lc_0))   ,
-                                       5 => real_voltage(get_inductor_current(emi_lc_1))    ,
-                                       6 => real_voltage(get_capacitor_voltage(emi_lc_1))
+                                       1 => to_real(get_inductor_current(primary_lc))  ,
+                                       2 => to_real(get_capacitor_voltage(primary_lc)) ,
+                                       3 => to_real(get_inductor_current(emi_lc_0))    ,
+                                       4 => to_real(get_capacitor_voltage(emi_lc_0))   ,
+                                       5 => to_real(get_inductor_current(emi_lc_1))    ,
+                                       6 => to_real(get_capacitor_voltage(emi_lc_1))
                                    ));
             end if;
 
-            inductor_current <= real_voltage(get_inductor_current(emi_lc_1));
-            output_voltage   <= real_voltage(get_capacitor_voltage(emi_lc_1));
+            -- inductor_current <= to_real(get_inductor_current(emi_lc_1));
+            -- output_voltage   <= to_real(get_capacitor_voltage(emi_lc_1));
 
         end if; -- rising_edge
     end process stimulus;	
