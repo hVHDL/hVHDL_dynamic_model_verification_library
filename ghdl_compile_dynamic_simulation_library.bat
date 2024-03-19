e55dba01787ef1197137bee44a3db61e76dec31f
Jari Honkanen <jaarhonka@yahoo.com>
2024-03-19 17:53:07 +0200

    update syntax checking

--- ghdl_compile_dynamic_simulation_library.bat
+++ ghdl_compile_dynamic_simulation_library.bat
@@ -1,41 +1,27 @@
 echo off
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
-SET project_root=%%F
+
+if "%1"=="" (
+    set src=./
+) else (
+    set src=%1
 )
-SET source=%project_root%/
 
-rem call %source%/hVHDL_math_library/ghdl_compile_math_library.bat
+call source/hVHDL_fixed_point/ghdl_compile_math_library.bat source/hVHDL_fixed_point
 
-ghdl -a --ieee=synopsys --std=08 %source%/simulator_utilities/write_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/real_to_fixed/real_to_fixed_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/simulator_utilities/write_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/multiplier/multiplier_base_types_18bit_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/multiplier/multiplier_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/sincos/sincos_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/half_bridge_model/half_bridge_pkg.vhd
 
-
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/division/division_internal_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/division/division_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/first_order_filter/first_order_filter_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/pi_controller/pi_controller_pkg.vhd
-
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/abc_to_ab_transform/ab_to_abc_transform_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd
-
-ghdl -a --ieee=synopsys --std=08 %source%/half_bridge_model/half_bridge_pkg.vhd
-
-ghdl -a --ieee=synopsys --std=08 %source%/state_variable/state_variable_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/lcr_filter_model/lcr_filter_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/buck_simulation_model/filtered_buck_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/state_variable/state_variable_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/lcr_filter_model/lcr_filter_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/buck_simulation_model/filtered_buck_model_pkg.vhd
 
 
-ghdl -a --ieee=synopsys --std=08 %source%/inverter_model/inverter_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/power_supply_model/psu_inverter_simulation_models_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/power_supply_model/power_supply_simulation_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/inverter_model/inverter_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/power_supply_model/psu_inverter_simulation_models_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/power_supply_model/power_supply_simulation_model_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/ac_motor_models/pmsm_electrical_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/ac_motor_models/pmsm_mechanical_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/ac_motor_models/permanent_magnet_motor_model_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/ac_motor_models/pmsm_electrical_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/ac_motor_models/pmsm_mechanical_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/ac_motor_models/permanent_magnet_motor_model_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %src%/ac_motor_models/field_oriented_motor_control/field_oriented_motor_control_pkg.vhd
