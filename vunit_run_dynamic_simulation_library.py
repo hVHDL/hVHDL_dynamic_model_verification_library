17dea21d297367b841ae585d90a8c5209a98c40a
Jari Honkanen <jaarhonka@yahoo.com>
2024-03-19 18:03:18 +0200

    add microprogram processor sources to vunit_run

--- vunit_run_dynamic_simulation_library.py
+++ vunit_run_dynamic_simulation_library.py
@@ -71,4 +71,52 @@ math_library_26x26.add_source_files(ROOT / "testbenches/converter_models/cascade
 math_library_26x26.add_source_files(ROOT / "testbenches/buck/buck_converter_tb.vhd")
 math_library_26x26.add_source_files(ROOT / "testbenches/buck/filtered_buck_synthesizable_tb.vhd")
 
+float = VU.add_library("float")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_type_definitions/float_word_length_24_bit_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_type_definitions/float_type_definitions_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_arithmetic_operations/*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/normalizer/normalizer_configuration/normalizer_with_1_stage_pipe_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/denormalizer/denormalizer_configuration/denormalizer_with_1_stage_pipe_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/normalizer/*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/denormalizer/*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_to_real_conversions" / "*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_adder/*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_multiplier/*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_alu/*.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_floating_point/float_first_order_filter/*.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_fixed_point/real_to_fixed/real_to_fixed_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_fixed_point/multiplier/multiplier_base_types_20bit_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_fixed_point/multiplier/configuration/multiply_with_1_input_and_output_registers_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_fixed_point/multiplier/multiplier_base_types_20bit_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_fixed_point/multiplier/multiplier_pkg.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/processor_configuration/float_processor_ram_width_pkg.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_memory_library/multi_port_ram/multi_port_ram_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_memory_library/multi_port_ram/ram_read_x2_write_x1.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_memory_library/multi_port_ram/arch_sim_read_x2_write_x1.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_memory_library/multi_port_ram/ram_read_x4_write_x1.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/source/hVHDL_memory_library/multi_port_ram/arch_sim_read_x4_write_x1.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/processor_configuration/processor_configuration_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/vhdl_assembler/microinstruction_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/vhdl_assembler/float_assembler_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/simple_processor/test_programs_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/processor_configuration/float_pipeline_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/simple_processor/simple_processor_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/memory_processor/memory_processing_pkg.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/memory_processor/memory_processor.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/simple_processor/float_example_program_pkg.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/testbenches/float_processor_tb.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/testbenches/memory_processor/memory_processor_pipeline_tb.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/testbenches/memory_processor/memory_processor_tb.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/testbenches/memory_processor/lcr_simulation_tb.vhd")
+
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/testbenches/memory_processor/lcr_3ph_tb.vhd")
+float.add_source_files(ROOT / "source/hVHDL_microprogam_processor/testbenches/memory_processor/lcr_simulation_rk4_tb.vhd")
+
 VU.main()
