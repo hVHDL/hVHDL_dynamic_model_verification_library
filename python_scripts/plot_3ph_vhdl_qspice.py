beed949f4c7acabc3dc1a5320bdaf768bbecbbd4
Jari Honkanen <jaarhonka@yahoo.com>
2024-03-23 20:14:31 +0200

    add plot_3ph_vhdl_qspice.py to plot 3 phase qspice vs vhdl simulation

--- python_scripts/plot_3ph_vhdl_qspice.py
+++ python_scripts/plot_3ph_vhdl_qspice.py
@@ -1,7 +1,6 @@
 from PyQSPICE import clsQSPICE as pqs
 
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 #make this file root for relative paths
@@ -9,7 +8,7 @@ import os
 path_to_this_file = os.path.dirname(os.path.realpath(__file__))
 
 #change directory to the lc filter directory
-pqs.chdir(path_to_this_file + './testbenches_mcu_models/3ph_lc')
+pqs.chdir(path_to_this_file + '/../testbenches_mcu_models/3ph_lc')
 
 run = pqs('3ph_lc.qsch')
 
@@ -23,7 +22,6 @@ run.setNline(4999)
 df = run.LoadQRAW(["V(uc1)", "V(uc2)","V(uc3)","I(L1)","I(L2)","I(L3)"])
 
 # load vhdl simulation data
-vhdl_data = pd.read_csv(path_to_this_file + '/vunit_out/lcr_3ph_tb.dat', delim_whitespace=True)
 
 fig1, (axT, axB) = plt.subplots(2,1,sharex=True,constrained_layout=True)
 
@@ -33,6 +31,8 @@ df.plot(ax=axT, x="Time",  y="V(uc3)", label="V(uc3)")
 df.plot(ax=axB, x="Time",  y="I(L1)", label="I(L1)")
 df.plot(ax=axB, x="Time",  y="I(L2)", label="I(L2)")
 df.plot(ax=axB, x="Time",  y="I(L3)", label="I(L3)")
+
+vhdl_data = pd.read_csv(path_to_this_file + '/../vunit_out/lcr_3ph_tb.dat', delim_whitespace=True)
 
 vhdl_data.plot(ax=axT, x="time", y="rkv1", label="rk4 uc1")
 vhdl_data.plot(ax=axT, x="time", y="rkv2", label="rk4 uc2")
