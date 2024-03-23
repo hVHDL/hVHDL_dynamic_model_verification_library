b74737033cd848a9b507c8ab9781f74c3986b5a7
Jari Honkanen <jaarhonka@yahoo.com>
2024-03-23 18:08:02 +0200

    remove test qspice file

--- test_pyqspice.py
+++ test_pyqspice.py
@@ -1,11 +1,8 @@
 from PyQSPICE import clsQSPICE as pqs
 
-# import re
-# import math
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
 
 #make this file root for relative paths
 import os
@@ -23,7 +20,7 @@ run.setNline(4999)
 
 df = run.LoadQRAW(["V(vout)", "I(L1)"])
 
-fig2, (axT, axB) = plt.subplots(2,1,sharex=True,constrained_layout=True)
+fig1, (axT, axB) = plt.subplots(2,1,sharex=True,constrained_layout=True)
 
 df.plot(ax=axT, x="Time",  y="V(vout)", label="V(vout)")
 df.plot(ax=axB, x="Time",  y="I(L1)", label="I(L1)")
